cb6193021b80e662d5240658894ac76f77ae07a9
Chang-Man <changagim@snu.ac.kr>
2022-09-13 22:58:47 +0900

    signup view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import UserCreateSerializer
+from django.db import IntegrityError
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+class UserSignUpView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            user, jwt_token = serializer.save()
+        except IntegrityError:
+            raise status.HTTP_409_CONFLICT
+
+        return Response({
+            'user': user.username,
+            'token': jwt_token
+        }, status=status.HTTP_201_CREATED)
